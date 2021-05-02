bfef870d379f6efb9206c0e4b93ae5bed4a2d8fd
Jonathon Lin <jonathonlin93@yahoo.com>
2021-05-02 09:58:07 -0700

    test

--- utils.py
+++ utils.py
@@ -133,6 +133,7 @@ def search_span_endpoints(start_probs, end_probs, args, context, question, windo
     max_start_index = start_probs.index(max(start_probs))
     max_end_index = -1
     max_joint_prob = 0.
+    print(context)
 
 
     if args.task == 1:
